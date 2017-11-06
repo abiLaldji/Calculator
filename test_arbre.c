c1ccc312f309814bc13fb966111385a5938496a2
denniscachau <dennis.cachau@gmail.com>
2017-11-06 09:25:56 +0100

    abibi2

--- test_arbre.c
+++ test_arbre.c
@@ -1,10 +1,12 @@
 
 
-#include <stdio.h>
+#include <stdio.h
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
 #include <math.h>
+#include "struct.h"
+#include "fonctions.h"
 
 typedef char * string;
 
